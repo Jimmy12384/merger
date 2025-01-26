9cdc4378bca12a60cd681d491922a4929dacf345
jimmy12384 <jimmyfens@gmail.com>
2025-01-25 19:24:03 -0500

    added state management support

--- game/logic.ts
+++ game/logic.ts
@@ -1,23 +1,25 @@
+import LobbyCreation from "./states/LobbyCreation";
+
 // util for easy adding logs
 const addLog = (message: string, logs: GameState["log"]): GameState["log"] => {
-  return [{ dt: new Date().getTime(), message: message }, ...logs].slice(
-    0,
-    MAX_LOG_SIZE
-  );
+	return [{ dt: new Date().getTime(), message: message }, ...logs].slice(
+		0,
+		MAX_LOG_SIZE
+	);
 };
 
 // If there is anything you want to track for a specific user, change this interface
 export interface User {
-  id: string;
+	id: string;
 }
 
 // Do not change this! Every game has a list of users and log of actions
-interface BaseGameState {
-  users: User[];
-  log: {
-    dt: number;
-    message: string;
-  }[];
+export interface BaseGameState {
+	users: User[];
+	log: {
+		dt: number;
+		message: string;
+	}[];
 }
 
 // Do not change!
@@ -35,63 +37,52 @@ export type DefaultAction = { type: "UserEntered" } | { type: "UserExit" };
 
 // This interface holds all the information about your game
 export interface GameState extends BaseGameState {
-  target: number;
+	state: String;
+	resolveAction: (action: GameAction) => GameState;
 }
 
 // This is how a fresh new game starts out, it's a function so you can make it dynamic!
 // In the case of the guesser game we start out with a random target
-export const initialGame = () => ({
-  users: [],
-  target: Math.floor(Math.random() * 100),
-  log: addLog("Game Created!", []),
-});
+export const initialGame = () =>
+	LobbyCreation({
+		users: [],
+		log: addLog("Game Created!", []),
+	});
 
 // Here are all the actions we can dispatch for a user
-type GameAction = { type: "guess"; guess: number };
+export type GameAction = { type: string; value: object };
 
 export const gameUpdater = (
-  action: ServerAction,
-  state: GameState
+	action: ServerAction,
+	state: GameState
 ): GameState => {
-  // This switch should have a case for every action type you add.
+	switch (action.type) {
+		case "UserEntered":
+			return {
+				...state,
+				users: [...state.users, action.user],
+				log: addLog(`user ${action.user.id} joined 🎉`, state.log),
+			};
 
-  // "UserEntered" & "UserExit" are defined by default
+		case "UserExit":
+			return {
+				...state,
+				users: state.users.filter((user) => user.id !== action.user.id),
+				log: addLog(`user ${action.user.id} left 😢`, state.log),
+			};
 
-  // Every action has a user field that represent the user who dispatched the action,
-  // you don't need to add this yourself
-  switch (action.type) {
-    case "UserEntered":
-      return {
-        ...state,
-        users: [...state.users, action.user],
-        log: addLog(`user ${action.user.id} joined 🎉`, state.log),
-      };
-
-    case "UserExit":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.user.id),
-        log: addLog(`user ${action.user.id} left 😢`, state.log),
-      };
-
-    case "guess":
-      if (action.guess === state.target) {
-        return {
-          ...state,
-          target: Math.floor(Math.random() * 100),
-          log: addLog(
-            `user ${action.user.id} guessed ${action.guess} and won! 👑`,
-            state.log
-          ),
-        };
-      } else {
-        return {
-          ...state,
-          log: addLog(
-            `user ${action.user.id} guessed ${action.guess}`,
-            state.log
-          ),
-        };
-      }
-  }
+		// All other actions should be a game action
+		default:
+			try {
+				return state.resolveAction(action as GameAction);
+			} catch (e) {
+				return {
+					...state,
+					log: addLog(
+						`user ${action.user.id} performed an invalid action! They are sus.`,
+						state.log
+					),
+				};
+			}
+	}
 };
